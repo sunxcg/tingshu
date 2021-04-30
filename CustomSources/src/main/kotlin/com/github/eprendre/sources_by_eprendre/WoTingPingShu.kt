0ec6a70784134eaf4b4f51a0d9b80607ee025be2
eprendre <eprendre0413@gmail.com>
2021-05-01 00:25:31 +0800

    fix: 我听评书网的域名更换

--- CustomSources/src/main/kotlin/com/github/eprendre/sources_by_eprendre/WoTingPingShu.kt
+++ CustomSources/src/main/kotlin/com/github/eprendre/sources_by_eprendre/WoTingPingShu.kt
@@ -58,7 +58,7 @@ object WoTingPingShu : TingShu(), IAd, AudioUrlExtraHeaders {
     }
 
     override fun getUrl(): String {
-        return "https://m.5tps.com"
+        return "https://m.5tps.vip"
     }
 
     override fun getName(): String {
@@ -83,7 +83,7 @@ object WoTingPingShu : TingShu(), IAd, AudioUrlExtraHeaders {
 
     override fun search(keywords: String, page: Int): Pair<List<Book>, Int> {
         val encodedKeywords = URLEncoder.encode(keywords, "gb2312")
-        val url = "https://m.5tps.com/so.asp?keyword=$encodedKeywords&page=$page"
+        val url = "https://m.5tps.vip/so.asp?keyword=$encodedKeywords&page=$page"
         val doc = Jsoup.connect(url).sslSocketFactory(socketFactory()).config().get()
 
         var totalPage = 1
@@ -125,7 +125,7 @@ object WoTingPingShu : TingShu(), IAd, AudioUrlExtraHeaders {
             val script = Jsoup.connect(iUrl).sslSocketFactory(socketFactory()).headers(mapOf(
                 "accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
                 "accept-encoding" to "gzip, deflate, br",
-                "referer" to "https://m.5tps.com/",
+                "referer" to "https://m.5tps.vip/",
                 "sec-fetch-dest" to "iframe",
                 "sec-fetch-mode" to "navigate",
                 "sec-fetch-site" to "cross-site",
@@ -168,53 +168,53 @@ object WoTingPingShu : TingShu(), IAd, AudioUrlExtraHeaders {
     override fun getCategoryMenus(): List<CategoryMenu> {
         val menu1 =
             CategoryMenu("评书分类", listOf(
-                CategoryTab("单田芳", "https://m.5tps.com/m_l_hot/1_1.html"),
-                CategoryTab("刘兰芳", "https://m.5tps.com/m_l_hot/2_1.html"),
-                CategoryTab("田连元", "https://m.5tps.com/m_l_hot/3_1.html"),
-                CategoryTab("袁阔成", "https://m.5tps.com/m_l_hot/4_1.html"),
-                CategoryTab("连丽如", "https://m.5tps.com/m_l_hot/5_1.html"),
-                CategoryTab("张少佐", "https://m.5tps.com/m_l_hot/6_1.html"),
-                CategoryTab("田战义", "https://m.5tps.com/m_l_hot/7_1.html"),
-                CategoryTab("孙一", "https://m.5tps.com/m_l_hot/8_1.html"),
-                CategoryTab("石连君", "https://m.5tps.com/m_l_hot/29_1.html"),
-                CategoryTab("马长辉", "https://m.5tps.com/m_l_hot/25_1.html"),
-                CategoryTab("王军", "https://m.5tps.com/m_l_hot/27_1.html"),
-                CategoryTab("王玥波", "https://m.5tps.com/m_l_hot/28_1.html"),
-                CategoryTab("王封臣", "https://m.5tps.com/m_l_hot/30_1.html"),
-                CategoryTab("关永超", "https://m.5tps.com/m_l_hot/35_1.html"),
-                CategoryTab("昊儒", "https://m.5tps.com/m_l_hot/26_1.html"),
-                CategoryTab("粤语评书", "https://m.5tps.com/m_l_hot/12_1.html"),
-                CategoryTab("其他评书", "https://m.5tps.com/m_l/13_1.html"))
+                CategoryTab("单田芳", "https://m.5tps.vip/m_l_hot/1_1.html"),
+                CategoryTab("刘兰芳", "https://m.5tps.vip/m_l_hot/2_1.html"),
+                CategoryTab("田连元", "https://m.5tps.vip/m_l_hot/3_1.html"),
+                CategoryTab("袁阔成", "https://m.5tps.vip/m_l_hot/4_1.html"),
+                CategoryTab("连丽如", "https://m.5tps.vip/m_l_hot/5_1.html"),
+                CategoryTab("张少佐", "https://m.5tps.vip/m_l_hot/6_1.html"),
+                CategoryTab("田战义", "https://m.5tps.vip/m_l_hot/7_1.html"),
+                CategoryTab("孙一", "https://m.5tps.vip/m_l_hot/8_1.html"),
+                CategoryTab("石连君", "https://m.5tps.vip/m_l_hot/29_1.html"),
+                CategoryTab("马长辉", "https://m.5tps.vip/m_l_hot/25_1.html"),
+                CategoryTab("王军", "https://m.5tps.vip/m_l_hot/27_1.html"),
+                CategoryTab("王玥波", "https://m.5tps.vip/m_l_hot/28_1.html"),
+                CategoryTab("王封臣", "https://m.5tps.vip/m_l_hot/30_1.html"),
+                CategoryTab("关永超", "https://m.5tps.vip/m_l_hot/35_1.html"),
+                CategoryTab("昊儒", "https://m.5tps.vip/m_l_hot/26_1.html"),
+                CategoryTab("粤语评书", "https://m.5tps.vip/m_l_hot/12_1.html"),
+                CategoryTab("其他评书", "https://m.5tps.vip/m_l/13_1.html"))
             )
 
         val menu2 = CategoryMenu("有声小说", listOf(
-            CategoryTab("玄幻奇幻", "https://m.5tps.com/m_l/46_1.html"),
-            CategoryTab("恐怖惊悚", "https://m.5tps.com/m_l/14_1.html"),
-            CategoryTab("言情通俗", "https://m.5tps.com/m_l/19_1.html"),
-            CategoryTab("武侠小说", "https://m.5tps.com/m_l/11_1.html"),
-            CategoryTab("历史军事", "https://m.5tps.com/m_l/15_1.html"),
-            CategoryTab("刑侦反腐", "https://m.5tps.com/m_l/16_1.html"),
-            CategoryTab("官场商战", "https://m.5tps.com/m_l/17_1.html"),
-            CategoryTab("人物纪实", "https://m.5tps.com/m_l/18_1.html"),
-            CategoryTab("有声文学", "https://m.5tps.com/m_l/10_1.html"),
-            CategoryTab("童话寓言", "https://m.5tps.com/m_l/20_1.html"),
-            CategoryTab("广播剧", "https://m.5tps.com/m_l/36_1.html"),
-            CategoryTab("英文读物", "https://m.5tps.com/m_l/22_1.html"))
+            CategoryTab("玄幻奇幻", "https://m.5tps.vip/m_l/46_1.html"),
+            CategoryTab("恐怖惊悚", "https://m.5tps.vip/m_l/14_1.html"),
+            CategoryTab("言情通俗", "https://m.5tps.vip/m_l/19_1.html"),
+            CategoryTab("武侠小说", "https://m.5tps.vip/m_l/11_1.html"),
+            CategoryTab("历史军事", "https://m.5tps.vip/m_l/15_1.html"),
+            CategoryTab("刑侦反腐", "https://m.5tps.vip/m_l/16_1.html"),
+            CategoryTab("官场商战", "https://m.5tps.vip/m_l/17_1.html"),
+            CategoryTab("人物纪实", "https://m.5tps.vip/m_l/18_1.html"),
+            CategoryTab("有声文学", "https://m.5tps.vip/m_l/10_1.html"),
+            CategoryTab("童话寓言", "https://m.5tps.vip/m_l/20_1.html"),
+            CategoryTab("广播剧", "https://m.5tps.vip/m_l/36_1.html"),
+            CategoryTab("英文读物", "https://m.5tps.vip/m_l/22_1.html"))
         )
 
         val menu3 = CategoryMenu("综艺节目", listOf(
-            CategoryTab("今日头条", "https://m.5tps.com/m_l/40_1.html"),
-            CategoryTab("商业财经", "https://m.5tps.com/m_l/42_1.html"),
-            CategoryTab("脱口秀", "https://m.5tps.com/m_l/41_1.html"),
-            CategoryTab("亲子教育", "https://m.5tps.com/m_l/43_1.html"),
-            CategoryTab("教育培训", "https://m.5tps.com/m_l/44_1.html"),
-            CategoryTab("百家讲坛", "https://m.5tps.com/m_l/9_1.html"),
-            CategoryTab("综艺娱乐", "https://m.5tps.com/m_l/34_1.html"),
-            CategoryTab("相声小品", "https://m.5tps.com/m_l/21_1.html"),
-            CategoryTab("汽车音乐", "https://m.5tps.com/m_l/23_1.html"),
-            CategoryTab("时尚生活", "https://m.5tps.com/m_l/45_1.html"),
-            CategoryTab("戏曲", "https://m.5tps.com/m_l/38_1.html"),
-            CategoryTab("二人转", "https://m.5tps.com/m_l/31_1.html"))
+            CategoryTab("今日头条", "https://m.5tps.vip/m_l/40_1.html"),
+            CategoryTab("商业财经", "https://m.5tps.vip/m_l/42_1.html"),
+            CategoryTab("脱口秀", "https://m.5tps.vip/m_l/41_1.html"),
+            CategoryTab("亲子教育", "https://m.5tps.vip/m_l/43_1.html"),
+            CategoryTab("教育培训", "https://m.5tps.vip/m_l/44_1.html"),
+            CategoryTab("百家讲坛", "https://m.5tps.vip/m_l/9_1.html"),
+            CategoryTab("综艺娱乐", "https://m.5tps.vip/m_l/34_1.html"),
+            CategoryTab("相声小品", "https://m.5tps.vip/m_l/21_1.html"),
+            CategoryTab("汽车音乐", "https://m.5tps.vip/m_l/23_1.html"),
+            CategoryTab("时尚生活", "https://m.5tps.vip/m_l/45_1.html"),
+            CategoryTab("戏曲", "https://m.5tps.vip/m_l/38_1.html"),
+            CategoryTab("二人转", "https://m.5tps.vip/m_l/31_1.html"))
         )
         return listOf(menu1, menu2, menu3)
     }
