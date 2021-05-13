2f91a73cecb476e502a24113696c067f2878166a
eprendre <eprendre0413@gmail.com>
2021-05-13 22:32:29 +0800

    fix: 修复听中国

--- CustomSources/src/main/kotlin/com/github/eprendre/sources_by_eprendre/TingChina.kt
+++ CustomSources/src/main/kotlin/com/github/eprendre/sources_by_eprendre/TingChina.kt
@@ -28,32 +28,32 @@ object TingChina : TingShu(), AudioUrlExtraHeaders {
     override fun getCategoryMenus(): List<CategoryMenu> {
         val menu1 = CategoryMenu(
             "有声小说", listOf(
-                CategoryTab("玄幻奇幻", "http://www.tingchina.com/yousheng/leip_135_1.htm"),
-                CategoryTab("网络热门", "http://www.tingchina.com/yousheng/leip_146_1.htm"),
-                CategoryTab("科幻有声", "http://www.tingchina.com/yousheng/leip_128_1.htm"),
-                CategoryTab("武侠小说", "http://www.tingchina.com/yousheng/leip_133_1.htm"),
-                CategoryTab("都市言情", "http://www.tingchina.com/yousheng/leip_125_1.htm"),
-                CategoryTab("鬼故事", "http://www.tingchina.com/yousheng/leip_129_1.htm"),
-                CategoryTab("历史军事", "http://www.tingchina.com/yousheng/leip_130_1.htm"),
-                CategoryTab("官场商战", "http://www.tingchina.com/yousheng/leip_126_1.htm"),
-                CategoryTab("刑侦推理", "http://www.tingchina.com/yousheng/leip_134_1.htm"),
-                CategoryTab("经典纪实", "http://www.tingchina.com/yousheng/leip_127_1.htm"),
-                CategoryTab("通俗文学", "http://www.tingchina.com/yousheng/leip_132_1.htm"),
-                CategoryTab("人物传记", "http://www.tingchina.com/yousheng/leip_131_1.htm")
+                CategoryTab("玄幻奇幻", "http://www.tingchina.com/yousheng/lei_135_1.htm"),
+                CategoryTab("网络热门", "http://www.tingchina.com/yousheng/lei_146_1.htm"),
+                CategoryTab("科幻有声", "http://www.tingchina.com/yousheng/lei_128_1.htm"),
+                CategoryTab("武侠小说", "http://www.tingchina.com/yousheng/lei_133_1.htm"),
+                CategoryTab("都市言情", "http://www.tingchina.com/yousheng/lei_125_1.htm"),
+                CategoryTab("鬼故事", "http://www.tingchina.com/yousheng/lei_129_1.htm"),
+                CategoryTab("历史军事", "http://www.tingchina.com/yousheng/lei_130_1.htm"),
+                CategoryTab("官场商战", "http://www.tingchina.com/yousheng/lei_126_1.htm"),
+                CategoryTab("刑侦推理", "http://www.tingchina.com/yousheng/lei_134_1.htm"),
+                CategoryTab("经典纪实", "http://www.tingchina.com/yousheng/lei_127_1.htm"),
+                CategoryTab("通俗文学", "http://www.tingchina.com/yousheng/lei_132_1.htm"),
+                CategoryTab("人物传记", "http://www.tingchina.com/yousheng/lei_131_1.htm")
             )
         )
 
         val menu2 = CategoryMenu(
             "评书大全", listOf(
-                CategoryTab("单田芳", "http://www.tingchina.com/pingshu/leip_136_1.htm"),
-                CategoryTab("田连元", "http://www.tingchina.com/pingshu/leip_141_1.htm"),
-                CategoryTab("袁阔成", "http://www.tingchina.com/pingshu/leip_143_1.htm"),
-                CategoryTab("连丽如", "http://www.tingchina.com/pingshu/leip_137_1.htm"),
-                CategoryTab("张少佐", "http://www.tingchina.com/pingshu/leip_145_1.htm"),
-                CategoryTab("孙一", "http://www.tingchina.com/pingshu/leip_140_1.htm"),
-                CategoryTab("田战义", "http://www.tingchina.com/pingshu/leip_142_1.htm"),
-                CategoryTab("粤语评书", "http://www.tingchina.com/pingshu/leip_144_1.htm"),
-                CategoryTab("其他评书", "http://www.tingchina.com/pingshu/leip_139_1.htm")
+                CategoryTab("单田芳", "http://www.tingchina.com/pingshu/lei_136_1.htm"),
+                CategoryTab("田连元", "http://www.tingchina.com/pingshu/lei_141_1.htm"),
+                CategoryTab("袁阔成", "http://www.tingchina.com/pingshu/lei_143_1.htm"),
+                CategoryTab("连丽如", "http://www.tingchina.com/pingshu/lei_137_1.htm"),
+                CategoryTab("张少佐", "http://www.tingchina.com/pingshu/lei_145_1.htm"),
+                CategoryTab("孙一", "http://www.tingchina.com/pingshu/lei_140_1.htm"),
+                CategoryTab("田战义", "http://www.tingchina.com/pingshu/lei_142_1.htm"),
+                CategoryTab("粤语评书", "http://www.tingchina.com/pingshu/lei_144_1.htm"),
+                CategoryTab("其他评书", "http://www.tingchina.com/pingshu/lei_139_1.htm")
 
             )
         )
@@ -129,8 +129,8 @@ object TingChina : TingShu(), AudioUrlExtraHeaders {
 
     override fun getCategoryList(url: String): Category {
         val doc = Jsoup.connect(url).config(true).get()
-        val pages = doc.selectFirst(".yema span").children()
-        val currentPage = Regex(".+leip_\\d+_(\\d+)\\.htm").find(url)!!.groupValues[1].toInt()
+        val pages = doc.selectFirst(".yema > span").children()
+        val currentPage = Regex(".+lei_\\d+_(\\d+)\\.htm").find(url)!!.groupValues[1].toInt()
         var totalPage = currentPage
         if (pages.last().absUrl("href") != url) {
             totalPage = currentPage + 1
@@ -142,39 +142,41 @@ object TingChina : TingShu(), AudioUrlExtraHeaders {
         }
 
         val list = ArrayList<Book>()
-        val elementList = doc.select(".showlist dl")
+        val elementList = doc.select(".zimulist > dl")
         elementList.forEach { element ->
-            val coverUrl = element.selectFirst("dt a img").absUrl("src")
-            val titleElement = element.selectFirst("dd .title a")
+            val coverUrl = element.selectFirst("dt > a > img").absUrl("src")
+            val titleElement = element.selectFirst("dd > a")
             val bookUrl = titleElement.absUrl("href")
-            val status: String
-            val (title, author, artist) = titleElement.text().split(" ").let {
-                val i = it[0].replace("《", "").replace("》", "")
-                val j = if (it.size > 2) it[1] else "" //大于2代表不是评书
-                val k = if (it.size > 2) {
-                    if (it.size > 3) {
-                        val temp = StringBuilder()
-                        (2 until (it.size - 1)).forEach { index ->
-                            temp.append(it[index])
-                            temp.append(" ")
-                        }
-                        temp.append(it.last().split("　")[0])
-                        temp.toString()
-                    } else {
-                        it[2].split("　")[0]
-                    }
-                } else ""
-
-                status = if (it.size > 2) {
-                    it.last().split("　")[1]
-                } else{
-                    it[1]
-                }
-                Triple(i, j, k)
-            }
-            val intro = element.selectFirst("dd .info").ownText()
+            val title = titleElement.text().trim()
+            val status = element.selectFirst("dd").ownText().split("　")[0]
+            val author = ""
+            val artist = ""
+//            val (title, author, artist) = titleElement.text().split(" ").let {
+//                val i = it[0].replace("《", "").replace("》", "")
+//                val j = if (it.size > 2) it[1] else "" //大于2代表不是评书
+//                val k = if (it.size > 2) {
+//                    if (it.size > 3) {
+//                        val temp = StringBuilder()
+//                        (2 until (it.size - 1)).forEach { index ->
+//                            temp.append(it[index])
+//                            temp.append(" ")
+//                        }
+//                        temp.append(it.last().split("　")[0])
+//                        temp.toString()
+//                    } else {
+//                        it[2].split("　")[0]
+//                    }
+//                } else ""
+//
+//                status = if (it.size > 2) {
+//                    it.last().split("　")[1]
+//                } else{
+//                    it[1]
+//                }
+//                Triple(i, j, k)
+//            }
+//            val intro = element.selectFirst("dd .info").ownText()
             list.add(Book(coverUrl, bookUrl, title, author, artist).apply {
-                this.intro = intro
                 this.status = status
                 this.sourceId = getSourceId()
             })
