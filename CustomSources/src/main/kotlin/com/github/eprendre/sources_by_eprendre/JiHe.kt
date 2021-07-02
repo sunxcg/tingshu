403eacf4e3312761b341fcb42d3ba1a463a8167d
eprendre <eprendre0413@gmail.com>
2021-07-03 01:34:39 +0800

    refactor: 机核网增加播单

--- CustomSources/src/main/kotlin/com/github/eprendre/sources_by_eprendre/JiHe.kt
+++ CustomSources/src/main/kotlin/com/github/eprendre/sources_by_eprendre/JiHe.kt
@@ -1,13 +1,22 @@
 package com.github.eprendre.sources_by_eprendre
 
 import com.github.eprendre.tingshu.extensions.config
+import com.github.eprendre.tingshu.extensions.getMobileUA
 import com.github.eprendre.tingshu.sources.AudioUrlDirectExtractor
 import com.github.eprendre.tingshu.sources.AudioUrlExtractor
 import com.github.eprendre.tingshu.sources.TingShu
 import com.github.eprendre.tingshu.utils.*
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.json.responseJson
 import org.jsoup.Jsoup
+import java.lang.Exception
+import kotlin.math.ceil
 
 object JiHe: TingShu(){
+    private val headers = mapOf(
+        "User-Agent" to getMobileUA()
+    )
+
     override fun getSourceId(): String {
         return "c6672f095d1248b58898c379d8989306"
     }
@@ -39,7 +48,8 @@ object JiHe: TingShu(){
     override fun getCategoryMenus(): List<CategoryMenu> {
         val menu1 = CategoryMenu(
             "机核", listOf(
-                CategoryTab("电台", "https://www.gcores.com/radios?page=1")
+                CategoryTab("电台", "https://www.gcores.com/radios?page=1"),
+                CategoryTab("播单", "https://www.gcores.com/gapi/v1/albums?page[limit]=12&page[offset]=0&sort=-updated-at&filter[is-on-sale]=0"),
             )
         )
 
@@ -47,38 +57,100 @@ object JiHe: TingShu(){
     }
 
     override fun getCategoryList(url: String): Category {
-        val doc = Jsoup.connect(url).config(false).get()
-        val totalPage = 132
-        val currentPage = doc.selectFirst(".pagination_item.is_active > a").text().toInt()
-        val nextUrl = doc.selectFirst(".pagination_next > a").absUrl("href")
+        if (url.contains("/radios")) {
+            val doc = Jsoup.connect(url).config(false).get()
+            val totalPage = 132
+            val currentPage = doc.selectFirst(".pagination_item.is_active > a").text().toInt()
+            val nextUrl = doc.selectFirst(".pagination_next > a").absUrl("href")
 
-        val list = ArrayList<Book>()
-        val elementList = doc.select(".original-radio")
-        elementList.forEach { element ->
-            val coverUrl = element.selectFirst(".original_imgArea").attr("style")
-                .replace("background-image:url(", "")
-                .replace(")", "")
-            val bookUrl = element.selectFirst(".am_card_inner > .am_card_content.original_content").absUrl("href")
-            val title = element.selectFirst(".am_card_title").text()
-            val author = ""
-            val artist = ""
-            val status = element.selectFirst(".avatar_text > h3").text()
-            list.add(Book(coverUrl, bookUrl, title, author, artist).apply {
-                this.status = status
-                this.sourceId = getSourceId()
-            })
+            val list = ArrayList<Book>()
+            val elementList = doc.select(".original-radio")
+            elementList.forEach { element ->
+                val coverUrl = element.selectFirst(".original_imgArea").attr("style")
+                    .replace("background-image:url(", "")
+                    .replace(")", "")
+                val bookUrl = element.selectFirst(".am_card_inner > .am_card_content.original_content").absUrl("href")
+                val title = element.selectFirst(".am_card_title").text()
+                val author = ""
+                val artist = ""
+                val status = element.selectFirst(".avatar_text > h3").text()
+                list.add(Book(coverUrl, bookUrl, title, author, artist).apply {
+                    this.status = status
+                    this.sourceId = getSourceId()
+                })
+            }
+
+            return Category(list, currentPage, totalPage, url, nextUrl)
+        } else {
+            val jsonObject = Fuel.get(url).header(headers).responseJson().third.get().obj()
+            val totalPage = ceil(jsonObject.getJSONObject("meta").getInt("record-count") / 12.0).toInt()
+            val currentPage = Regex("\\[offset\\]=(\\d+)").find(url)!!.groupValues[1].toInt() / 12 + 1
+            var nextUrl = ""
+            if (currentPage < totalPage) {
+                nextUrl = "https://www.gcores.com/gapi/v1/albums?page[limit]=12&page[offset]=${currentPage * 12}&sort=-updated-at&filter[is-on-sale]=0"
+            }
+
+            val list = ArrayList<Book>()
+            val results = jsonObject.getJSONArray("data")
+            (0 until results.length()).forEach {
+                val result = results.getJSONObject(it)
+                val id = result.getString("id")
+                val attributes = result.getJSONObject("attributes")
+                val coverUrl = "https://image.gcores.com/${attributes.getString("cover")}?x-oss-process=image/resize,limit_1,m_fill,w_360,h_360/quality,q_90"
+                val bookUrl = "https://www.gcores.com/gapi/v1/albums/${id}/published-radios?page[limit]=100&page[offset]=0&include=media,category,albums"
+                val author = ""
+                val artist = ""
+                val title = attributes.getString("title")
+                list.add(Book(coverUrl, bookUrl, title, author, artist).apply {
+                    this.sourceId = getSourceId()
+                })
+            }
+            return Category(list, currentPage, totalPage, url, nextUrl)
         }
-
-        return Category(list, currentPage, totalPage, url, nextUrl)
     }
 
     override fun getBookDetailInfo(bookUrl: String, loadEpisodes: Boolean, loadFullPages: Boolean): BookDetail {
         val episodes = ArrayList<Episode>()
         if (loadEpisodes) {
-            val doc = Jsoup.connect(bookUrl).config(false).get()
-            val downloadUrl = doc.selectFirst("svg[data-icon=download]").parent().absUrl("href")
-            Episode("1", downloadUrl).apply {
-                episodes.add(this)
+            if (bookUrl.contains("/radios")) {
+                val doc = Jsoup.connect(bookUrl).config(false).get()
+                val downloadUrl = doc.selectFirst("svg[data-icon=download]").parent().absUrl("href")
+                Episode("1", downloadUrl).apply {
+                    episodes.add(this)
+                }
+            } else {
+                val jsonObject = Fuel.get(bookUrl).header(headers).responseJson().third.get().obj()
+                val data = jsonObject.getJSONArray("data")
+                val included = jsonObject.getJSONArray("included")
+                val count = jsonObject.getJSONObject("meta").getInt("record-count")
+
+                val audioUrlMap = HashMap<String, String>()
+                (0 until included.length()).forEach {
+                    val jsonObj = included.getJSONObject(it)
+                    val id = jsonObj.getString("id")
+                    val attributes = jsonObj.getJSONObject("attributes")
+                    if (attributes.has("audio")) {
+                        val audioUrl = "https://alioss.gcores.com/uploads/audio/${attributes.getString("audio")}"
+                        audioUrlMap[id] = audioUrl
+                    }
+                }
+                (0 until data.length()).forEach {
+                    try {
+                        val jsonObj = data.getJSONObject(it)
+                        val id = jsonObj.getString("id")
+                        val attributes = jsonObj.getJSONObject("attributes")
+                        val title = attributes.getString("title")
+                        val mediaId = jsonObj.getJSONObject("relationships")
+                            .getJSONObject("media")
+                            .getJSONObject("data")
+                            .getString("id")
+                        Episode(title, audioUrlMap[mediaId]!!).apply {
+                            episodes.add(this)
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
             }
         }
         return BookDetail(episodes)
